07bae03b4442671b0f9f9a4bb3b1508633534885
sinshutu <c0114292a2@edu.teu.ac.jp>
2015-06-03 11:02:43 +0900

    add suzuki/app directory

---
+++ app/src/main/java/com/example/suzuki/slot/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.suzuki.slot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class MainActivity extends Activity {
+    private TextView[] slotNum = new TextView[3];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            slotNum[0] = (TextView) findViewById(R.id.slot0);
+            slotNum[1] = (TextView) findViewById(R.id.slot1);
+            slotNum[2] = (TextView) findViewById(R.id.slot2);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if(event.getAction() == event.ACTION_DOWN) {
+            startSlot();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void startSlot(){
+        String[] slotValue = getResources().getStringArray(R.array.slot_num);
+        Random r = new Random();
+        for(int i=0;i<slotNum.length;i++){
+            slotNum[i].setText(slotValue[r.nextInt(4)]);
+        }
+    }
+}
